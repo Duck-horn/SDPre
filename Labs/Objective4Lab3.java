f9b023812b715d01e6476a230d739c41ed6db056
Ving <ving4@pm.me>
2022-02-09 19:47:50 -0500

    complete Objective4Lab3

---
+++ Labs/Objective4Lab3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Objective4Lab3 {
+  public static void main(String[] args) {
+    int birthYear, age, currentYear;
+
+    Scanner keyboard = new Scanner(System.in);
+
+
+    birthYear = 0;
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("How old will you be this year?");
+    age = keyboard.nextInt();
+    currentYear = 2022;
+    birthYear = currentYear - age;
+    System.out.println("You were born in" + birthYear);
+    input.close();
+  }
+}
