9813cedcd6502c58728f8128fb82787276619a87
Ving <ving4@pm.me>
2022-02-10 22:03:24 -0500

    Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,16 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+
+int i, s;
+
+    for (i = 1; i < 11; i++){
+  System.out.println(i);
+  }
+
+    for (s = 10; s > 0; s-=1){
+    System.out.println(s);
+   }
+
+  }
+
+}
