3b2d8ab6f0e279e8830e34283aa67afe6a9e5ba0
Ving <ving4@pm.me>
2022-02-10 22:13:21 -0500

    Objective8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,13 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int control;
+    int sum = 0;
+
+      for (control = 1; control < 21; control++){
+  sum = sum + control;
+    }
+
+  System.out.println("The total is " + sum);
+
+    }
+}
